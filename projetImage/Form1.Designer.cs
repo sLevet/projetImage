fe3255b3f090bc3e9806333445bb2a18b1b1aefe
sLevet <info@levet.net>
2013-05-18 09:17:54 +0200

    First commit

---
+++ projetImage/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace projetImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_loadPicture = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_saveInDb = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_loadPicture
+            // 
+            this.button_loadPicture.Location = new System.Drawing.Point(963, 82);
+            this.button_loadPicture.Name = "button_loadPicture";
+            this.button_loadPicture.Size = new System.Drawing.Size(75, 23);
+            this.button_loadPicture.TabIndex = 0;
+            this.button_loadPicture.Text = "Load picture";
+            this.button_loadPicture.UseVisualStyleBackColor = true;
+            this.button_loadPicture.Click += new System.EventHandler(this.button_loadPicture_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(44, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 433);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(953, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(967, 137);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 3;
+            this.button_save.Text = "save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // button_saveInDb
+            // 
+            this.button_saveInDb.Location = new System.Drawing.Point(971, 257);
+            this.button_saveInDb.Name = "button_saveInDb";
+            this.button_saveInDb.Size = new System.Drawing.Size(75, 23);
+            this.button_saveInDb.TabIndex = 4;
+            this.button_saveInDb.Text = "save in DB";
+            this.button_saveInDb.UseVisualStyleBackColor = true;
+            this.button_saveInDb.Click += new System.EventHandler(this.button_saveInDb_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 504);
+            this.Controls.Add(this.button_saveInDb);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_loadPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_loadPicture;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Button button_saveInDb;
+    }
+}
+
---
+++ projetImage/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projetImage
+{
+    public partial class Form1 : Form
+    {
+        //******************************************************
+        // vars
+        //*****************************************************
+        Bitmap map;
+        System.Drawing.Image Origin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //*******************************************************
+        // buttons
+        //***********************************************************
+        private void button_loadPicture_Click(object sender, EventArgs e)
+        {
+            LoadImage();
+        }
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            SaveImage();
+        }
+        //**********************************************************
+        //  functions
+        //**********************************************************
+        //
+        // load image from file
+        //
+        public void LoadImage()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            DialogResult dr = op.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string path = op.FileName;
+                pictureBox1.Load(path);
+                Bitmap temp = new Bitmap(pictureBox1.Image,
+                   new Size(pictureBox1.Width, pictureBox1.Height));
+                pictureBox1.Image = temp;
+                map = new Bitmap(pictureBox1.Image);
+                Origin = pictureBox1.Image;
+            }
+        }
+        //
+        // save image in local folder. Save in png format, our choice ( png is loseless and free ! )
+        //
+        public void SaveImage()
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            FolderBrowserDialog fl = new FolderBrowserDialog();
+            // I force a name if nothing writed in name's field
+            if (textBox1.Text.Length == 0)
+            {
+                textBox1.Text = "John Doe";
+            }
+            if (fl.ShowDialog() != DialogResult.Cancel)
+            {
+                pictureBox1.Image.Save(fl.SelectedPath + @"\" + textBox1.Text + @".png", System.Drawing.Imaging.ImageFormat.Png);
+            };
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button_saveInDb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
